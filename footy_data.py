4720dfc4cd998d4b5e054e057334177fcb4102f2
Mimi Flores <aflores393@cps.edu>
2024-10-16 10:58:42 -0500

    updated footy data

--- footy_data.py
+++ footy_data.py
@@ -1,12 +1,12 @@
 from typing import List, Tuple
 
 footy_db: List[Tuple[str, str, int, int, int]] = [
-    (
+    (    
         "pierre-emerick aubameyang",  # player
         "arsenal",  # team
         3138,  # minutes
         22,  # goals
-          3,  # assists
+        3,  # assists
 
     ),
    (
@@ -14,7 +14,7 @@ footy_db: List[Tuple[str, str, int, int, int]] = [
         "manchester city",  # team
          2660,  # minutes
         20,  # goals
-          1,  # assists
+        1,  # assists
 
     ),
 (
@@ -39,6 +39,111 @@ footy_db: List[Tuple[str, str, int, int, int]] = [
         2653,  # minutes
         17,  # goals
           7,  # assists
+
+    ),
+(
+        "anthony martial",  # player
+        "manchester united",  # team
+        2638,  # minutes
+        17,  # goals
+          6,  # assists
+
+    ),
+
+
+(
+        "sergio leonel agüero del castillo",  # player
+        "manchester city",  # team
+        1456,  #
+
+
+
+    ),
+
+
+(
+        "joel matip",  # player
+        "liverpool",  # team
+      704,  # minutes
+      1,  # goals
+        0,  # assists
+
+    ),
+
+
+(
+        "xherdan shaqiri",  # player
+        "liverpool",  # team
+      182,  # minutes
+      1,  # goals
+        0,  # assists
+
+    ),
+
+
+(
+        "kyle walker",  # player
+        "manchester city",  # team
+      2395,  # minutes
+      1,  # goals
+        4,  # assists
+
+    ),
+
+
+(
+        "aymeric laporte",  # player
+        "manchester city",  # team
+      1104,  # minutes
+     1 ,  # goals
+        0,  # assists
+
+    ),
+
+
+(
+        "cedric ricardo alves soares",  # player
+        "arsenal",  # team
+      1559,  # minutes
+      1,  # goals
+        1,  # assists
+
+    ),
+
+
+(
+        "reiss nelson",  # player
+        "arsenal",  # team
+      627,  # minutes
+      1,  # goals
+        0,  # assists
+
+    ),
+
+
+(
+        "granit xhaka",  # player
+        "arsenal",  # team
+      2591,  # minutes
+      1,  # goals
+        2,  # assists
+
+    ),
+(
+        "calum chambers",  # player
+        "arsenal",  # team
+   1103,  # minutes
+      1,  # goals
+        1,  # assists
+
+    ),
+
+(
+        "lucas torreira",  # player
+        "arsenal",  # team
+   1508,  # minutes
+      1,  # goals
+        1,  # assists
 
     )
 ]
